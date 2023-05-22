63137cde38383737b33b0895a6ce6520d64d0646
Рома <pomo4444ka@gmail.com>
2023-05-22 13:49:51 +0300

    initial commit

---
+++ src/main/java/com/steamtest/streamapiandoptional/service/EmployeeService.java
@@ -0,0 +1,52 @@
+package com.steamtest.streamapiandoptional.service;
+
+import com.steamtest.streamapiandoptional.database.Employee;
+import com.steamtest.streamapiandoptional.exceptions.EmployeeAlreadyAddedException;
+import com.steamtest.streamapiandoptional.exceptions.EmployeeNotFoundException;
+import com.steamtest.streamapiandoptional.exceptions.EmployeeStorageIsFullException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class EmployeeService {
+    private static final int sizeLimit = 10;
+    private final Map<String, Employee> employees = new HashMap<>(sizeLimit);
+
+    public Collection<Employee> getAll() {
+        return employees.values();
+    }
+
+    public Employee add(Employee employee) {
+        if (employees.size() >= sizeLimit) {
+            throw new EmployeeStorageIsFullException();
+        }
+        if (employees.containsKey(createEmployee(employee))) {
+            throw new EmployeeAlreadyAddedException();
+        }
+        employees.put(createEmployee(employee), employee);
+        return employee;
+    }
+
+    public Employee find(String firstName, String lastName) {
+        Employee employee = employees.get(createEmployee(firstName, lastName));
+        if (employee == null) {
+            throw new EmployeeNotFoundException();
+        }
+        return employee;
+    }
+
+    public Employee remove(String firstName, String lastName) {
+        return employees.remove(createEmployee(firstName, lastName));
+    }
+
+    private static String createEmployee(Employee employee) {
+        return createEmployee(employee.getFirstName(), employee.getLastName());
+    }
+
+    public static String createEmployee(String firstName, String lastName) {
+        return (firstName + lastName).toLowerCase();
+    }
+}
